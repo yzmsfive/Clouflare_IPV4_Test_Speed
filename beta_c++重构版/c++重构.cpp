f671a28595104942cedb6dec5287951b3c85b63e
yzmsfive <100995469+yzmsfive@users.noreply.github.com>
2022-04-18 14:18:08 +0800

    Add files via upload

---
+++ beta_c++重构版/c++重构.cpp
@@ -0,0 +1,25 @@
+﻿#include <cstdio>
+#include <iostream>
+#include <windows.system.h>
+
+int mod;
+
+void ipv4();
+void ipv6();
+
+int main()
+{
+    printf("菜单：\n1.IPV4优选测速\n2.IPV6优选测速\n请选择菜单：");
+    std::cin >> mod;
+    mod == 1 ? ipv4() : ipv6();
+    std::cout << "Hello World!\n";
+}
+
+void ipv4()
+{
+    system("CF优选IP_单IPV4.bat");
+}
+void ipv6()
+{
+    system("CF优选IP_单IPV6.bat");
+}
